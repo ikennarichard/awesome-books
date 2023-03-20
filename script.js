e50bf184e3ab3dd58e6489517f1705fae9cceddd
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-20 12:21:04 -1100

    Add books store

---
+++ script.js
@@ -0,0 +1,50 @@
+const booKTitle = document.querySelector('.book_title');
+const bookAuthor = document.querySelector('.book_author');
+const addBookBtn = document.querySelector('.addbook_btn');
+const bookDisplayScreen = document.querySelector('.book_display');
+
+const booksArray = localStorage.getItem('books_store') ? JSON.parse(localStorage.getItem('books_store')) : [];
+
+// add books to local storage
+
+
+function addBook() {
+  let title = booKTitle.value;
+  let author = bookAuthor.value;
+
+  let booksObj = {
+    title: title,
+    author: author,
+  }
+
+  booksArray.push(booksObj);
+  localStorage.setItem('books_store', JSON.stringify(booksArray));
+
+  displayBook(title, author)
+}
+
+//remove book from list
+function removeBook(i) {
+  booksArray.splice(i, 1)
+}
+
+function displayBook(title, author) {
+  bookDisplayScreen.innerHTML+=`
+  <div class='book_details'>
+    <p>${title}</p>
+    <p>${author}</p>
+    <button type='button' class='remove_book'>Remove</button>
+    <hr/>
+  </div>
+  `
+  const details = [...document.querySelectorAll('.book_details')];
+
+  const books = [...document.querySelectorAll('.remove_book')];
+
+  books.forEach((item, i) => item.addEventListener('click', () => {
+    details[i].remove();
+    removeBook(i)
+  }));
+}
+
+addBookBtn.addEventListener('click', addBook)
