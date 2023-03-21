c653ab8a43ee80f85343278b2d550adbb202f48b
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-20 20:43:45 -1100

    Fix eslint issues

--- script.js
+++ script.js
@@ -1,70 +1,63 @@
 const bookDisplayScreen = document.querySelector('.book_display');
 const form = document.querySelector('form');
+
 localStorage.setItem('book_store', JSON.stringify([]));
+
 const bookList = JSON.parse(localStorage.getItem('book_store'));
 
 function updateStorage() {
-  localStorage.setItem('book_store', JSON.stringify(bookList))
+  localStorage.setItem('book_store', JSON.stringify(bookList));
+}
+
+function setListItems(arr) {
+  let listItems = '';
+  for (let i = 0; i < arr.length; i += 1) {
+    listItems += `
+    <li>${arr[i].title}</li>
+    <li>${arr[i].author}</li>
+    <button type='button' class='remove_book'>Remove</button>
+    <hr/>
+    `;
+  }
+  return listItems;
+}
+
+function displayBooks() {
+  bookDisplayScreen.innerHTML = `
+  <ul class='book_details'>
+  ${setListItems(bookList)}
+  </ul>
+  `;
+}
+
+function removeBook(i) {
+  bookList.splice(i, 1);
+  updateStorage();
+  displayBooks();
+}
+
+document.querySelectorAll('.remove_book').forEach((btn, i) => btn.addEventListener('click', () => {
+  removeBook(i);
+}));
+
+function addToBookList(item) {
+  bookList.push(item);
 }
 
 function addBook(e) {
-  e.preventDefault()
+  e.preventDefault();
 
   const booKTitleValue = document.querySelector('.book_title').value;
   const booKAuthorValue = document.querySelector('.book_author').value;
 
-  book = {
+  const book = {
     title: booKTitleValue,
     author: booKAuthorValue,
-  }
+  };
 
   addToBookList(book);
-  updateStorage()
-  displayBooks();
-}
-
-//add to book list
-
-function addToBookList(item) {
-  bookList.push(item)
-}
-
-//remove book from list
-function removeBook(i) {
-  bookList.splice(i, 1);
   updateStorage();
-  displayBooks()
-}
-
-// set list for iteme to display
-
-function setListItems(arr) {
-  let items = ''
-  for (let i=0; i < arr.length; i++) {
-    items += `
-    <li>${arr[i].title}</li>
-    <li>${arr[i].author}</li>
-    <button type='button' class='remove_book' onclick='removeBook(${i})'>Remove</button>
-    <hr/>
-    `
-  }
-  console.log(items)
-  return items;
-}
-
-
-function displayBooks() {
-  bookDisplayScreen.innerHTML=`
-  <ul class='book_details'>
-  ${setListItems(bookList)}
-  </ul>
-  `
-  activateTemp();
-}
-
-function activateTemp() {
-  const details = [...document.querySelectorAll('.book_details')];
-  const removeBookBtn = [...document.querySelectorAll('.remove_book')];
+  displayBooks();
 }
 
 form.addEventListener('submit', (e) => addBook(e));
